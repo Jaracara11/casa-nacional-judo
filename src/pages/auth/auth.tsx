d7b0211861d081f310209a3b6e34073f118f81e8
Luis_Eduardo <leduardo@evopointsolution.com>
2022-09-01 17:33:53 -0400

    .

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -1,13 +1,17 @@
-import { FormControl, InputLabel, FormHelperText, Input } from '@mui/material';
-
 const Auth = () => {
   return (
     <>
-      <FormControl>
-        <InputLabel htmlFor='my-input'>Email address</InputLabel>
-        <Input id='my-input' aria-describedby='my-helper-text' />
-        <FormHelperText id='my-helper-text'>We'll never share your email.</FormHelperText>
-      </FormControl>
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Excepturi nam quas quos expedita molestias modi, eum soluta vero aliquam quisquam
+      ratione dolorum magnam fuga. Id aliquam laboriosam maiores iure animi a sed velit quo repudiandae nesciunt nisi totam, aliquid consequatur
+      soluta ipsa, voluptatibus incidunt sequi voluptate. Eligendi voluptate dolorem nihil eos, magni commodi beatae, sunt, dolor veritatis porro
+      fugit mollitia dignissimos fugiat explicabo! Doloribus natus numquam possimus. Alias optio sequi iste enim ratione repudiandae? Quos voluptas
+      nemo unde quibusdam hic necessitatibus sunt fugiat? Vel quod error sint quam incidunt animi quia quasi hic atque odio, nisi in nostrum nihil!
+      Animi eaque fugit, enim non, sapiente pariatur voluptatum aliquam quam laborum numquam quaerat? Quo eaque molestias numquam dicta, nobis
+      consectetur vero esse voluptatibus aperiam pariatur debitis aliquid architecto dolore illo accusamus voluptatem cupiditate maiores magnam! Quod
+      numquam omnis soluta tempore expedita alias doloribus modi maiores. Exercitationem maxime dolorem pariatur. Beatae hic, ea natus odit vero
+      ducimus accusamus commodi, nisi provident aliquam harum. Ab, aut. Aliquid vel tempora omnis modi dolore labore neque ducimus doloribus autem
+      soluta. Vitae enim expedita iste blanditiis! Inventore, dignissimos dolorem amet magni cupiditate deserunt tempora quasi? Ipsum ad modi saepe
+      placeat illo blanditiis distinctio, molestiae culpa cupiditate.
     </>
   );
 };
