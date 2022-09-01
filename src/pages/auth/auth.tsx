cb4a7d36bfbbfb51c5c9838883b02764fdbb7f92
Luis_Eduardo <leduardo@evopointsolution.com>
2022-08-31 20:04:28 -0400

    .

--- src/pages/auth/auth.tsx
+++ src/pages/auth/auth.tsx
@@ -1,5 +1,15 @@
+import { FormControl, InputLabel, FormHelperText, Input } from '@mui/material';
+
 const Auth = () => {
-  return <p>Login</p>;
+  return (
+    <>
+      <FormControl>
+        <InputLabel htmlFor='my-input'>Email address</InputLabel>
+        <Input id='my-input' aria-describedby='my-helper-text' />
+        <FormHelperText id='my-helper-text'>We'll never share your email.</FormHelperText>
+      </FormControl>
+    </>
+  );
 };
 
 export default Auth;
