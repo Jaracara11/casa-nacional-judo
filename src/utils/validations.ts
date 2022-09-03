aaee46e200a90fc23d4ffd939a5ae56000637c3d
Luis_Eduardo <leduardo@evopointsolution.com>
2022-09-03 19:38:16 -0400

    completo member validation

--- src/utils/validations.ts
+++ src/utils/validations.ts
@@ -1,11 +1,11 @@
 import * as Yup from 'yup';
 
 const signInValidation = Yup.object({
-  email: Yup.string().email().required('Campo obligatorio!'),
+  email: Yup.string().email().required('Campo obligatorio.'),
   password: Yup.string()
     .max(20, 'Contraseña no puede exceder 20 caracteres.')
     .min(6, 'Contraseña no puede tener menos de 6 caracteres.')
-    .required('Campo obligatorio!')
+    .required('Campo obligatorio.')
 });
 
 const memberValidation = Yup.object({
@@ -20,18 +20,17 @@ const memberValidation = Yup.object({
   birthDate: Yup.string().required('Campo requerido.'),
   bloodType: Yup.string(),
   identification: Yup.string().max(11, 'Número de identificación no puede exceder de 11 caracteres.'),
-  address: Yup.string().max(50, 'Dirección no puede exceder de 50 caracteres.').required('Campo requerido.')
+  address: Yup.string().max(50, 'Dirección no puede exceder de 50 caracteres.').required('Campo requerido.'),
+  phone1: Yup.string()
+    .test('Número de telefono no puede exceder de 10 caracteres.', (val) => val?.length === 10)
+    .required('Campo requerido.'),
+  phone2: Yup.string().test('Número de telefono no puede exceder de 10 caracteres.', (val) => val?.length === 10),
+  email: Yup.string().email(),
+  belt: Yup.string(),
+  signUpDate: Yup.string(),
+  monthlyFee: Yup.number().positive(),
+  anualFee: Yup.number().positive(),
+  totalAmountDue: Yup.number()
 });
 
 export { signInValidation, memberValidation };
-
-interface IMember {
-  phone1: string;
-  phone2?: string;
-  email?: string;
-  belt?: string;
-  signUpDate?: string;
-  monthlyFee?: number;
-  anualFee?: number;
-  totalAmountDue?: number;
-}
