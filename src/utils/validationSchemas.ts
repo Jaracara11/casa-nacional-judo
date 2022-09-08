175acbd083aa31d45d14ab5e687f2e3ac6346e90
Luis_Eduardo <leduardo@evopointsolution.com>
2022-09-08 18:49:34 -0400

    correccion yup

--- src/utils/validationSchemas.ts
+++ src/utils/validationSchemas.ts
@@ -1,40 +1,36 @@
-import { object, string, number, mixed } from 'yup';
+import * as Yup from 'yup';
 
-const signInValidation = object({
-  email: string().email().required('Campo obligatorio.'),
-  password: string()
+const signInValidation = Yup.object({
+  email: Yup.string().email().required('Campo obligatorio.'),
+  password: Yup.string()
     .max(20, 'Contraseña no puede exceder 20 caracteres.')
     .min(6, 'Contraseña no puede tener menos de 6 caracteres.')
     .required('Campo obligatorio.')
 });
 
-const memberValidation = object({
-  firstName: string()
+const memberValidation = Yup.object({
+  firstName: Yup.string()
     .max(20, 'Nombre no puede exceder de 20 caracteres.')
     .min(3, 'Nombre no puede tener menos de 3 caracteres.')
     .required('Campo requerido.'),
-  lastName: string()
+  lastName: Yup.string()
     .max(25, 'Apellido no puede exceder de 25 caracteres.')
     .min(3, 'Apellido no puede tener menos de 3 caracteres.')
     .required('Campo requerido.'),
-  birthDate: string().required('Campo requerido.'),
-  bloodType: string(),
-  identification: string().max(11, 'Número de identificación no puede exceder de 11 caracteres.'),
-  address: string().max(50, 'Dirección no puede exceder de 50 caracteres.').required('Campo requerido.'),
-  phone1: string()
+  birthDate: Yup.string().required('Campo requerido.'),
+  bloodType: Yup.string(),
+  identification: Yup.string().max(11, 'Número de identificación no puede exceder de 11 caracteres.'),
+  address: Yup.string().max(50, 'Dirección no puede exceder de 50 caracteres.').required('Campo requerido.'),
+  phone1: Yup.string()
     .test('Número de teléfono no puede exceder de 10 caracteres.', (val) => val?.length === 10)
     .required('Campo requerido.'),
-  phone2: string().test('Número de teléfono no puede exceder de 10 caracteres.', (val) => val?.length === 10),
-  email: string().email(),
-  belt: string(),
-  signUpDate: string(),
-  monthlyFee: number().positive(),
-  anualFee: number().positive(),
-  totalAmountDue: number(),
-  attachment: mixed().test('fileSize', 'The file is too large', (value) => {
-    if (!value.length) return true; // attachment is optional
-    return value[0].size <= 2000000;
-  })
+  phone2: Yup.string().test('Número de teléfono no puede exceder de 10 caracteres.', (val) => val?.length === 10),
+  email: Yup.string().email(),
+  belt: Yup.string(),
+  signUpDate: Yup.string(),
+  monthlyFee: Yup.number().positive(),
+  anualFee: Yup.number().positive(),
+  totalAmountDue: Yup.number()
 });
 
 export { signInValidation, memberValidation };
