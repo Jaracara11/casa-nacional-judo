2a8d2f78a4ee20ea446b92bcfe233b4788814541
Luis_Eduardo <leduardo@evopointsolution.com>
2022-09-25 12:34:43 -0400

    .

--- src/services/members.service.ts
+++ src/services/members.service.ts
@@ -4,36 +4,36 @@ import { db } from '../utils/firebase';
 
 const membersCollection = 'members';
 const collectionRef = (colRef: string) => {
-  return collection(db, colRef);
+    return collection(db, colRef);
 };
 
 const docRef = (id: string, colRef: string) => {
-  return doc(db, colRef, id);
+    return doc(db, colRef, id);
 };
 
 const getAllMembers = async () => {
-  const q = query(collectionRef(membersCollection), orderBy('firstName'));
-  const data = await getDocs(q);
-  return data.docs.map((doc: any) => ({ ...doc.data(), id: doc.id }));
+    const q = query(collectionRef(membersCollection), orderBy('firstName'));
+    const data = await getDocs(q);
+    return data.docs.map((doc: any) => ({ ...doc.data(), id: doc.id }));
 };
 
 const getMemberById = async (id: string) => {
-  const docSnap = await getDoc(docRef(id, membersCollection));
-  return docSnap.data() as IMember;
+    const docSnap = await getDoc(docRef(id, membersCollection));
+    return docSnap.data() as IMember;
 };
 
 const createMember = async (data: IMember) => {
-  const newMember = await addDoc(collectionRef(membersCollection), data);
-  return newMember;
+    const newMember = await addDoc(collectionRef(membersCollection), data);
+    return newMember;
 };
 
 const updateMember = async (data: IMember) => {
-  const member = await updateDoc(docRef(data.id!, membersCollection), { data });
-  return member;
+    const member = await updateDoc(docRef(data.id!, membersCollection), { data });
+    return member;
 };
 
 const deleteMember = async (id: string) => {
-  await deleteDoc(docRef(id, membersCollection));
+    await deleteDoc(docRef(id, membersCollection));
 };
 
 export { getAllMembers, getMemberById, createMember, updateMember, deleteMember };
