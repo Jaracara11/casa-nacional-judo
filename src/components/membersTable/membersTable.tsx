8640b9b52d34e179f9018ab647c2f4168985fd74
Luis_Eduardo <leduardo@evopointsolution.com>
2022-09-25 11:00:30 -0400

    .

--- src/components/membersTable/membersTable.tsx
+++ src/components/membersTable/membersTable.tsx
@@ -7,53 +7,53 @@ import { Spinner } from '../spinner/spinner';
 import { NavigateBtn } from '../buttons/navigateButton/navigateBtn';
 
 export const MembersTable = () => {
-  const [loadingData, setLoadingData] = useState(true);
-  const [members, setMembers] = useState<IMember[]>([]);
+    const [loadingData, setLoadingData] = useState(true);
+    const [members, setMembers] = useState<IMember[]>([]);
 
-  useEffect(() => {
-    const loadMembers = async () => {
-      await getAllMembers()
-        .then((response) => {
-          setMembers(response);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
-      setLoadingData(false);
-    };
-    loadMembers();
-  }, []);
+    useEffect(() => {
+        const loadMembers = async () => {
+            await getAllMembers()
+                .then((response) => {
+                    setMembers(response);
+                })
+                .catch((err) => {
+                    console.log(err);
+                });
+            setLoadingData(false);
+        };
+        loadMembers();
+    }, []);
 
-  return loadingData ? (
-    <Spinner />
-  ) : (
-    <Table striped bordered hover>
-      <thead>
-        <tr>
-          <th>Nombre</th>
-          <th>Cedula</th>
-          <th>Fecha De Nacimiento</th>
-          <th>Teléfono</th>
-          <th>Grado</th>
-          <th>Mensualidad</th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {members.map((x: IMember) => (
-          <tr key={x.id}>
-            <td>{`${x.firstName} ${x.lastName}`}</td>
-            <td>{x.identification}</td>
-            <td>{x.birthDate}</td>
-            <td>{x.phone1}</td>
-            <td>{x.belt}</td>
-            <td>{x.totalAmountDue}</td>
-            <td>
-              <NavigateBtn route={`/edit-member/${x.id}`} variant='btn btn-success btn-sm' text={'Mas Información'} />
-            </td>
-          </tr>
-        ))}
-      </tbody>
-    </Table>
-  );
+    return loadingData ? (
+        <Spinner />
+    ) : (
+        <Table striped bordered hover>
+            <thead>
+                <tr>
+                    <th>Nombre</th>
+                    <th>Cedula</th>
+                    <th>Fecha De Nacimiento</th>
+                    <th>Teléfono</th>
+                    <th>Grado</th>
+                    <th>Mensualidad</th>
+                    <th></th>
+                </tr>
+            </thead>
+            <tbody>
+                {members.map((member: IMember) => (
+                    <tr key={member.id}>
+                        <td>{`${member.firstName} ${member.lastName}`}</td>
+                        <td>{member.identification}</td>
+                        <td>{member.birthDate}</td>
+                        <td>{member.phone1}</td>
+                        <td>{member.belt}</td>
+                        <td>{member.totalAmountDue}</td>
+                        <td>
+                            <NavigateBtn route={`/edit-member/${member.id}`} variant='btn btn-success btn-sm' text={'Mas Información'} />
+                        </td>
+                    </tr>
+                ))}
+            </tbody>
+        </Table>
+    );
 };
