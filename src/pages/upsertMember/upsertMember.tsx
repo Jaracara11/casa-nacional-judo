7ef2be1dac2b90239b0da3ec8c834e5ebeb4986b
Luis_Eduardo <leduardo@evopointsolution.com>
2022-09-26 19:27:44 -0400

    agregando los campos al upsert

--- src/pages/upsertMember/upsertMember.tsx
+++ src/pages/upsertMember/upsertMember.tsx
@@ -81,20 +81,22 @@ export const UpsertMember = () => {
                 setLoadingData(true);
                 try {
                     if (params.id) {
-                        updateMember(values);
+                        console.log(values);
+                        //updateMember(values);
                         SwalObj.fire({
                             html: `<strong>Miembro Actualizado!</strong>`,
                             icon: 'info',
                             showConfirmButton: false
                         });
                     } else {
-                        createMember(values);
+                        console.log(values);
+                        //createMember(values);
                         SwalObj.fire({
                             html: `<strong>Nuevo miembro Agregado!</strong>`,
                             icon: 'success',
                             showConfirmButton: false
                         }).then(() => {
-                            navigate('/');
+                            //navigate('/');
                         });
                     }
                 } catch (err: any) {
@@ -123,8 +125,20 @@ export const UpsertMember = () => {
                     <ErrorView error={errors.firstName} />
                     <input className='form-control mt-1' {...register('lastName')} type='text' placeholder='Apellidos...' name='lastName' />
                     <ErrorView error={errors.lastName} />
-                    <input className='form-control mt-1' {...register('lastName')} type='date' placeholder='Apellidos...' name='lastName' />
-                    <ErrorView error={errors.lastName} />
+                    <input className='form-control mt-1' {...register('birthDate')} type='date' name='birthDate' />
+                    <ErrorView error={errors.birthDate} />
+                    <input className='form-control mt-1' {...register('bloodType')} type='text' placeholder='Tipo de sangre...' name='bloodType' />
+                    <ErrorView error={errors.bloodType} />
+                    <input className='form-control mt-1' {...register('identification')} type='text' placeholder='Cédula...' name='identification' />
+                    <ErrorView error={errors.identification} />
+                    <textarea className='form-control mt-1' {...register('address')} placeholder='Dirección...' name='address' />
+                    <ErrorView error={errors.address} />
+                    <input className='form-control mt-1' {...register('phone1')} type='text' placeholder='Teléfono 1...' name='phone1' />
+                    <ErrorView error={errors.phone1} />
+                    <input className='form-control mt-1' {...register('phone2')} type='text' placeholder='Teléfono 2...' name='phone2' />
+                    <ErrorView error={errors.phone2} />
+                    <input className='form-control mt-1' {...register('email')} type='email' placeholder='Email...' name='email' />
+                    <ErrorView error={errors.email} />
                     <label htmlFor='documentImage'>Foto de documento:</label>
                     <input
                         type='file'
