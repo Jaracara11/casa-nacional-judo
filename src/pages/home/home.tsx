b5f96553b5ea3715bf2ef11a6db8d868accd9b7c
Luis_Eduardo <leduardo@evopointsolution.com>
2022-09-27 17:11:22 -0400

    se agrega boton para ir a add-member

--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -1,9 +1,14 @@
 import { MembersTable } from '../../components/membersTable/membersTable';
+import { NavigateBtn } from '../../components/buttons/navigateButton/navigateBtn';
+import './home.css';
 
 export const Home = () => {
-  return (
-    <div>
-      <MembersTable />
-    </div>
-  );
+    return (
+        <div className='home-container'>
+            <div className='btn-panel'>
+                <NavigateBtn variant='primary' route='/add-member' text='Agregar Nuevo Miembro' />
+            </div>
+            <MembersTable />
+        </div>
+    );
 };
