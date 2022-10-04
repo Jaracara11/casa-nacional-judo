c48f7a077b4bb00ea5836f2f732361d2998be185
Luis_Eduardo <leduardo@evopointsolution.com>
2022-10-04 11:35:47 -0400

    .

--- src/components/memberModal/memberModal.tsx
+++ src/components/memberModal/memberModal.tsx
@@ -2,7 +2,6 @@ import Modal from 'react-bootstrap/Modal';
 import Button from 'react-bootstrap/Button';
 import ListGroup from 'react-bootstrap/ListGroup';
 import { IMember } from '../../interfaces/IMember';
-import { ListGroupItem } from 'react-bootstrap';
 import { getAgeFromDate } from '../../utils/helper';
 
 export const MemberModal = (props: any) => {
@@ -10,33 +9,39 @@ export const MemberModal = (props: any) => {
 
     return (
         <>
-            <Modal show={props.show} onHide={props.toggle} centered>
+            <Modal show={props.show} onHide={props.toggle} centered backdrop='static'>
                 <Modal.Header closeButton>
                     <Modal.Title>{member.firstName + ' ' + member.lastName}</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
                     <ListGroup className='list-group-flush'>
                         <ListGroup.Item>
-                            <strong>Peso:</strong> {member.weight} kg
+                            <strong>Peso:</strong> {member.weight}kg
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <strong>Edad:</strong> {getAgeFromDate(member.birthDate)}
                         </ListGroup.Item>
                         <ListGroup.Item>
                             <strong>Cinturón:</strong> {member.belt}
                         </ListGroup.Item>
                         <ListGroup.Item>
-                            <strong>Edad:</strong> {getAgeFromDate(member.birthDate)}
+                            <strong>Tipo de sangre:</strong> {member.bloodType}
+                        </ListGroup.Item>
+                        <ListGroup.Item>
+                            <strong>Identificación:</strong> {member.identification}
                         </ListGroup.Item>
                     </ListGroup>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button
-                        variant='secondary'
+                        variant='outline-dark'
                         onClick={() => {
                             props.toggle(false);
                         }}>
-                        Close
+                        Cerrar
                     </Button>
                     <Button variant='primary' onClick={() => {}}>
-                        Save Changes
+                        Editar
                     </Button>
                 </Modal.Footer>
             </Modal>
--- src/components/membersTable/membersTable.tsx
+++ src/components/membersTable/membersTable.tsx
@@ -50,7 +50,7 @@ export const MembersTable = () => {
                     {members.map((member: IMember) => (
                         <tr key={member.id}>
                             <td>{`${member.firstName} ${member.lastName}`}</td>
-                            <td>{member.weight} kg</td>
+                            <td>{member.weight}kg</td>
                             <td>{formatDate(member.birthDate)}</td>
                             <td>{member.phone1}</td>
                             <td>{member.belt}</td>
