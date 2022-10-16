01536f63cfb15ac6a83dfcbfc3f55bf6390110bc
Luis_Eduardo <leduardo@evopointsolution.com>
2022-10-16 15:43:34 -0400

    .

--- functions/index.js
+++ functions/index.js
@@ -2,10 +2,9 @@ const functions = require('firebase-functions');
 const admin = require('firebase-admin');
 admin.initializeApp();
 
-//delete all appointments from all collections at 12:00am
 exports.updateMonthlyFee = functions.pubsub
     //0 0 1 * *
-    .schedule('*/5 * * * *')
+    .schedule('* * * * *')
     .timeZone('America/Santo_Domingo')
     .onRun(async (context) => {
         const db = admin.firestore();
@@ -14,7 +13,7 @@ exports.updateMonthlyFee = functions.pubsub
 
         snap = await db.collection('members').get();
         snap.forEach((snap) => {
-            promises.push(snap.ref.delete());
+            promises.push(snap.ref.update({ monthlyFee: 777 }));
         });
 
         return Promise.all(promises);
